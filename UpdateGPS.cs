e28a5038883515001d7c81f8a7c07093e13f5e89
Amartya Dutta <56846056+BakaZoro@users.noreply.github.com>
2019-11-30 20:29:42 +0530

    Add files via upload

---
+++ UpdateGPS.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateGPS: MonoBehaviour {
+
+	public Text coord;
+
+	private void Update(){
+	
+		coord.text = "Latitude :" + GPS.Instance.latitude.ToString () + "   Longitude :" + GPS.Instance.longitude.ToString ();
+	}
+}
