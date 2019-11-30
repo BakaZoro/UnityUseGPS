d25f46350931158242fdd8c36df1f01d8859e4a4
Amartya Dutta <56846056+BakaZoro@users.noreply.github.com>
2019-11-30 20:29:25 +0530

    Add files via upload

---
+++ GPS.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPS : MonoBehaviour {
+
+	public static GPS Instance { set; get; }
+	public float latitude;
+	public float longitude;
+
+	private void Start(){
+
+		Instance = this;
+		DontDestroyOnLoad (gameObject);
+		StartCoroutine (StartLocationService ());
+	}
+
+	private IEnumerator StartLocationService(){
+
+		if (!Input.location.isEnabledByUser) {
+
+			Debug.Log ("Location Not Enabled");
+			yield break;
+		}
+
+		Input.location.Start();
+		int maxWait = 15;
+
+		while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0) {
+			yield return new WaitForSeconds (1);
+			maxWait--;
+
+		}
+
+		if (maxWait == 0) {
+			Debug.Log ("GPS Timed-Out");
+			yield break;
+		}
+
+		if (Input.location.status == LocationServiceStatus.Failed) {
+			Debug.Log ("Location Not Found");
+			yield break;
+		}
+
+		latitude = Input.location.lastData.latitude;
+		longitude = Input.location.lastData.longitude;
+
+		yield break;
+	}
+
+}
